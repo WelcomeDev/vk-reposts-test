415c70c99fc9290a70c0097ab8ebe74f5266bca0
Fedor Ishcheko <fedor.ishchenko.18@gmail.com>
2024-12-17 17:17:07 +0300

    fix my shit

--- src/app/api/vk/route.ts
+++ src/app/api/vk/route.ts
@@ -2,13 +2,13 @@ import { getDb, setDb } from '@/app/api/vk/storage';
 
 export async function POST(req: Request) {
     const body = await req.json();
-    console.log(body);
+    console.log(JSON.stringify(body));
     if (body.type === 'confirmation') {
         return new Response(process.env.CONFIRMATION_CODE);
     }
 
     const data = await getDb()
-    data.append(body);
+    data.push(body);
     await setDb(data);
 
     return new Response('ok')
